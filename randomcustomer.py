47e51fe11292ce1142c904d08034aaeac990f781
edwardwbli <edward.w.b.li@gmail.com>
2017-12-05 11:27:39 +0800

    five

--- randomcustomer.py
+++ randomcustomer.py
@@ -3,14 +3,14 @@ import matplotlib as mp
 mp.use('Agg')
 import matplotlib.pyplot as pl
 
-customer1 = np.random.random((12,))
-customer2 = np.random.random((12,))
-customer3 = np.random.random((12,))
-customer4 = np.random.random((12,))
+customer1 = np.random.random((12,100))*1000
+customer2 = np.random.random((12,100))*1000
+customer3 = np.random.random((12,100))*1000
+customer4 = np.random.random((12,100))*1000
 
-pl.plot(customer1*1000)
-pl.plot(customer2*5000)
-pl.plot(customer3*10000)
-pl.plot(customer4*20000)
+pl.plot(customer1)
+pl.plot(customer2)
+pl.plot(customer3)
+pl.plot(customer4)
 
-pl.savefig('book4_read.png')
+pl.savefig('book5_read.png')
