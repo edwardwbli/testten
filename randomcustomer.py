b53a5453ba64c487157e839a0455428be709f263
edwardwbli <edward.w.b.li@gmail.com>
2017-12-05 12:22:09 +0800

    night

--- randomcustomer.py
+++ randomcustomer.py
@@ -4,14 +4,11 @@ import numpy as np
 #mp.use('Agg') #disable using DISPLAY 
 #import matplotlib.pyplot as pl
 
-customer1 = np.random.random((12,100))*5000
-customer2 = np.random.random((12,100))*10000
-customer3 = np.random.random((12,100))*50000
-customer4 = np.random.random((12,100))*100000
+customer1 = np.random.random((100,12))*5000
 print(customer1)
 
 #pl.polar(customer1)
 #pl.savefig('book5_read.png')
 a = np.asarray(customer1)
-np.savetxt("c1.csv", a, delimiter=",")
+np.savetxt("c1.csv", a, fmt="%10.2f",delimiter=",")
 
