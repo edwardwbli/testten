1c634196d7159fc15714a849145a62e12830eb3f
edwardwbli <edward.w.b.li@gmail.com>
2017-12-05 11:33:41 +0800

    six

--- randomcustomer.py
+++ randomcustomer.py
@@ -1,16 +1,14 @@
+from __future__ import print_function
 import numpy as np
 import matplotlib as mp
 mp.use('Agg')
 import matplotlib.pyplot as pl
 
-customer1 = np.random.random((12,100))*1000
-customer2 = np.random.random((12,100))*1000
-customer3 = np.random.random((12,100))*1000
-customer4 = np.random.random((12,100))*1000
+customer1 = np.random.random((12,100))*5000
+customer2 = np.random.random((12,100))*10000
+customer3 = np.random.random((12,100))*50000
+customer4 = np.random.random((12,100))*100000
+print(customer1)
 
 pl.plot(customer1)
-pl.plot(customer2)
-pl.plot(customer3)
-pl.plot(customer4)
-
 pl.savefig('book5_read.png')
