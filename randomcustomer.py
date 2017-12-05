ee7b5441f2290f4390679e99ebb4cdbbe91161d5
edwardwbli <edward.w.b.li@gmail.com>
2017-12-05 12:54:41 +0800

    third

--- randomcustomer.py
+++ randomcustomer.py
@@ -4,13 +4,25 @@ import numpy as np
 #mp.use('Agg') #disable using DISPLAY 
 #import matplotlib.pyplot as pl
 
-customer1 = np.random.random((100,12))*10000
-target1 = np.ones((100,1)) * 2
+customer1 = np.random.random((100,12))*5000
+target1 = np.ones((100,1))
 sample1 = np.hstack((customer1,target1))
-print(sample1)
 
+customer2 = np.random.random((100,12))*10000
+target2 = np.ones((100,1)) * 2   
+sample2 = np.hstack((customer2,target2))
+
+customer3 = np.random.random((100,12))*20000
+target3 = np.ones((100,1)) * 3    
+sample3 = np.hstack((customer3,target3))
+
+customer4 = np.random.random((100,12))*50000
+target4 = np.ones((100,1))    
+sample4 = np.hstack((customer4,target4))
 #pl.polar(customer1)
 #pl.savefig('book5_read.png')
 a = np.asarray(sample1)
-np.savetxt("c1.csv", a, fmt="%10d",delimiter=",")
-
+np.savetxt("c1.csv", sample1, fmt="%10d",delimiter=",")
+np.savetxt("c2.csv", sample2, fmt="%10d",delimiter=",")
+np.savetxt("c3.csv", sample3, fmt="%10d",delimiter=",")
+np.savetxt("c4.csv", sample4, fmt="%10d",delimiter=",")
